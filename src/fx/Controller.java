aea2da2ea867b59b3a89ede67b62767d3f90f2fa
Eqnoxx <Eqnoxx@163.com>
2021-06-16 20:41:12 +0800

    add something

--- src/fx/Controller.java
+++ src/fx/Controller.java
@@ -1,4 +1,21 @@
 package fx;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML public Label mainScreen;
+    public void AC(ActionEvent event) {
+        mainScreen.setText("");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 }
