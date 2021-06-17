deab12978607081a0e92f896a1f33f3d8053be09
Eqnoxx <Eqnoxx@163.com>
2021-06-17 13:26:05 +0800

    add something

--- src/fx/Controller.java
+++ src/fx/Controller.java
@@ -10,74 +10,69 @@ import java.lang.String;
 import java.math.MathContext;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 public class Controller implements Initializable {
     final int MAXLEN = 20;
 
     @FXML public Label mainScreen;
     @FXML public Label secondScreen;
-    public void AC(ActionEvent event) {
+
+    BigDecimal num = BigDecimal.ZERO;
+    public void clear(ActionEvent event) {
         mainScreen.setText("");
     }
-    public void typeNumZero(ActionEvent event) {
+    private void appendNum(int dig) {
         String mainText = mainScreen.getText();
         if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"0");
+            mainScreen.setText(mainText+new Integer(dig).toString());
+            num = num.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(dig));
         }
+    }
+    private void appendSymbol(char sym) {
+
+    }
+    public void typeNumZero(ActionEvent event) {
+        appendNum(0);
     }
     public void typeNumOne(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"1");
-        }
+        appendNum(1);
     }
     public void typeNumTwo(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"2");
-        }
+        appendNum(2);
     }
     public void typeNumThree(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"3");
-        }
+        appendNum(3);
     }
     public void typeNumFour(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"4");
-        }
+        appendNum(4);
     }
     public void typeNumFive(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"5");
-        }
+        appendNum(5);
     }
     public void typeNumSix(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"6");
-        }
+        appendNum(6);
     }
     public void typeNumSeven(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"7");
-        }
+        appendNum(7);
     }
     public void typeNumEight(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"8");
-        }
+        appendNum(8);
     }
     public void typeNumNine(ActionEvent event) {
-        String mainText = mainScreen.getText();
-        if (mainText.length() < MAXLEN) {
-            mainScreen.setText(mainText+"9");
-        }
+        appendNum(9);
+    }
+    public void plus() {
+        appendSymbol();
+    }
+    public void minus() {
+
+    }
+    public void multiple() {
+
+    }
+    public void divide() {
+
     }
     public void equal(ActionEvent event) {
         String mainText = mainScreen.getText();
